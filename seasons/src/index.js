a660bf7c4e9123efe71372fb280f8c818e57cafc
Adrian Catalan <adrian.catalan@travel-ims.com>
2019-10-22 23:37:50 +0200

    [section4] Weather app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi!</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
