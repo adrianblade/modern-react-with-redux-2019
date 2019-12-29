7b1f5edd12eb3415f414ecc8109b23926bddef9d
Adrian Catalan <adrian.catalan@travel-ims.com>
2019-12-29 16:59:17 +0100

    Add streams project

---
+++ streams/client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import authReducer from './authReducer'
+import streamsReducer from './streamReducer'
+
+export default combineReducers({
+  auth: authReducer,
+  form: formReducer,
+  streams: streamsReducer
+})
